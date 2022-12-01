8901fd38fc517001d8246f133eafc71dac8769cc
carloss-ziegler <chziegler445@gmail.com>
2022-12-01 12:54:27 -0300

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
 import express from "express";
 import mongoose from "mongoose";
-
+import { Server } from "socket.io";
 import { router } from "./router";
 import path from "node:path";
+import http from "node:http";
+
+const app = express();
+const server = http.createServer(app);
+export const io = new Server(server);
 
 mongoose
   .connect("mongodb://localhost:27017")
   .then(() => {
-    const app = express();
-
+    app.use((req, res, next) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Access-Control-Allow-Methods", "*");
+      res.setHeader("Access-Control-Allow-Headers", "*");
+      next();
+    });
     app.use(
       "/uploads",
       express.static(path.resolve(__dirname, "..", "uploads"))
@@ -17,7 +26,7 @@ mongoose
     app.use(express.json());
     app.use(router);
 
-    app.listen(3001, () => {
+    server.listen(3001, () => {
       console.log("🎉 Funcionando em http://localhost:3001");
     });
   })
